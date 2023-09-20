af42999d2fdf200071941d7b5a0080973e080568
AkashQuo <akashbora10@gmail.com>
2023-09-20 03:18:11 -0700

    creating checkpoint

--- mysite/Gamers/models.py
+++ mysite/Gamers/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class GAMER_USER(models.Model):
+    id
     title = models.CharField(max_length=100)
     content = models.TextField()
 
--- mysite/Gamers/urls.py
+++ mysite/Gamers/urls.py
@@ -17,7 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from Gamers.views import post_list
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls')),
+    path('gamers_list/', post_list, name="gamers_list"),
 ]
